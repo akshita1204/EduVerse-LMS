98c56e5894c2a40ce42b6ccd5726e8f6bd62003a
akshita1204 <akshitatyagi04@gmail.com>
2025-02-08 11:00:46 +0530

    courses section addes

--- client/src/context/AddContext.jsx
+++ client/src/context/AddContext.jsx
@@ -1,9 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { dummyCourses } from "../assets/assets";
+import { useEffect } from "react";
 export const AppContext=createContext()
 export const AppContextProvider=(props)=>
 {
-    const value={
+    const currency=import.meta.env.VITE_CURRENCY
+    const [allCourses,setAllCourses]=useState([])
+    //fetch all courses
+    const fetchAllCourses=async()=>
+    {
+        setAllCourses(dummyCourses)
+    }
 
+    useEffect(()=>{fetchAllCourses()},[])
+
+    const value={
+          currency, allCourses
     }
     return (
         <AppContext.Provider value={value}>
