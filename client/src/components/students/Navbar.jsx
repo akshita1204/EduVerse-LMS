3193da4b3e53ccc0d2b65ee81e36f042658605bc
akshita1204 <akshitatyagi04@gmail.com>
2025-02-08 06:09:34 +0530

    Navigation Bar completed

--- client/src/components/students/Navbar.jsx
+++ client/src/components/students/Navbar.jsx
@@ -17,8 +17,12 @@ const Navbar = () => {
 
     <div className='hidden md:flex items-center gap-5 text-gray-500'>
       <div className='flex items-center gap-5'>
+          { user && 
+          <>
           <button>Become Educator</button>
        |  <Link to='/my-enrollments'>My Enrollments</Link>
+          </>
+         }
       </div>
       { user ? <UserButton/> :
       <button onClick={()=>openSignIn()} className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create Account</button>}
@@ -27,11 +31,16 @@ const Navbar = () => {
 
     {/*For Phone Screen*/}
     <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
-       <div>
-         <button>Become Educator</button>
-     |  <Link to='/my-enrollments'>My Enrollments</Link>
+       <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+         {
+          user && 
+          <>
+          <button>Become Educator</button>
+          | <Link to='/my-enrollments'>My Enrollments</Link>
+          </>
+         }
        </div>
-       <button> <img src={assets.user_icon} alt=""/></button>
+         {user ? <UserButton/> : <button onClick={()=>openSignIn()}> <img src={assets.user_icon} alt=""/></button> }
        </div>
     </div>
     
