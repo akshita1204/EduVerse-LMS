0b2f9a7f33f9415690342de5033495bade26e4ff
akshita1204 <akshitatyagi04@gmail.com>
2025-02-08 11:28:59 +0530

    Course card set up

--- client/src/context/AddContext.jsx
+++ client/src/context/AddContext.jsx
@@ -1,10 +1,13 @@
 import { createContext, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 export const AppContext=createContext()
 export const AppContextProvider=(props)=>
 {
     const currency=import.meta.env.VITE_CURRENCY
+    const navigate=useNavigate()
+
     const [allCourses,setAllCourses]=useState([])
     //fetch all courses
     const fetchAllCourses=async()=>
@@ -12,10 +15,17 @@ export const AppContextProvider=(props)=>
         setAllCourses(dummyCourses)
     }
 
+    const calculateRating=(course)=>{
+        if(course.courseRatings.length===0) return 0;
+        let totalRating=0
+        course.courseRatings.forEach(rating=>{totalRating+=rating.rating})
+        return totalRating / course.courseRatings.length
+    }
+
     useEffect(()=>{fetchAllCourses()},[])
 
     const value={
-          currency, allCourses
+          currency, allCourses, navigate, calculateRating
     }
     return (
         <AppContext.Provider value={value}>
