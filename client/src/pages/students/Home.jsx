cd2a31f60eb3b8b1817edfcb5791f5c6d6e551a5
akshita1204 <akshitatyagi04@gmail.com>
2025-02-08 14:27:38 +0530

    Home Page Completed

--- client/src/pages/students/Home.jsx
+++ client/src/pages/students/Home.jsx
@@ -3,6 +3,8 @@ import Hero from '../../components/students/Hero'
 import Companies from '../../components/students/Companies'
 import CourseSection from '../../components/students/CourseSection'
 import TestimonialsSection from '../../components/students/TestimonialsSection'
+import CallToAction from '../../components/students/CallToAction'
+import Footer from '../../components/students/Footer'
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <Companies/>
       <CourseSection/>
       <TestimonialsSection/>
+      <CallToAction/>
+      <Footer/>
     </div>
   )
 }
