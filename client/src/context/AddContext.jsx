c20798fda9176f031634a0f0648f89b0e7006449
akshita1204 <akshitatyagi04@gmail.com>
2025-02-09 11:13:29 +0530

    Course Structure Updated

--- client/src/context/AddContext.jsx
+++ client/src/context/AddContext.jsx
@@ -2,6 +2,9 @@ import { createContext, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from 'humanize-duration'
+
+
 export const AppContext=createContext()
 export const AppContextProvider=(props)=>
 {
@@ -22,11 +25,43 @@ export const AppContextProvider=(props)=>
         course.courseRatings.forEach(rating=>{totalRating+=rating.rating})
         return totalRating / course.courseRatings.length
     }
+    
+    //function to calculate course chapter time
+    const calculateChapterTime=(chapter)=>
+    {
+       let time=0
+       chapter.chapterContent.map((lecture)=>time+=lecture.lectureDuration)
+       return humanizeDuration(time*60*1000, {units:["h","m"]})
+    }
+
+    //function to calculate the course duration 
+    const calculateCourseDuration=()=>
+    {
+       let time=0
+       course.courseContent.map((chapter)=>chapter.chapterContent.map((lecture)=>time+=lecture.lectureDuration))
+       return humanizeDuration(time*60*1000, {units:["h","m"]})
+    }
+
+    //function to calculatae the number of lectures in the course
+    const calculateNumberofLectures=(course)=>
+    {
+        let total=0
+        course.courseContent.forEach(chapter=>
+        {
+            if(Array.isArray(chapter.chapterContent))
+            {
+                total+=chapter.chapterContent.length;
+            }
+        }
+        );
+        return total;
+    }
+
 
     useEffect(()=>{fetchAllCourses()},[])
 
     const value={
-          currency, allCourses, navigate, calculateRating, isEducator, setIsEducator
+          currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateChapterTime, calculateCourseDuration, calculateNumberofLectures
     }
     return (
         <AppContext.Provider value={value}>
