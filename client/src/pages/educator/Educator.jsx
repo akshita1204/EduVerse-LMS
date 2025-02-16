2580dbb5d699d506fcdf009f2072facc7a0e0c7d
akshita1204 <akshitatyagi04@gmail.com>
2025-02-16 15:14:47 +0530

    Educator Navbar setup

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/educator/Navbar'
 
-const Educator = () => {
+const Educator= () => {
   return (
     <div className='text-default min-h-screen bg-white'>
       <Navbar/>
